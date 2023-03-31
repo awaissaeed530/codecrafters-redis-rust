af77e29cd929e610593d3695909dfa66d850e9d9
Awais Saeed <awaissaeed530@gmail.com>
2023-04-01 01:15:09 +0500

    respond to ping command

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
-use std::net::TcpListener;
+use std::{net::TcpListener, io::{Read, Write}};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut stream) => {
                 println!("accepted new connection");
+                let mut buf = [0; 512];
+                stream.read(&mut buf).unwrap();
+                stream.write("+PONG\r\n".as_bytes()).unwrap();
             }
             Err(e) => {
                 println!("error: {}", e);
