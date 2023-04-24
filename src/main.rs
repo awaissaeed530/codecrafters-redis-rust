a6c9903a30d4ade4d01e2634ef6033a99b4cdfc5
Awais Saeed <awaissaeed530@gmail.com>
2023-04-24 13:33:34 +0500

    handle concurrent connections

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,32 @@
-use std::{net::TcpListener, io::{Read, Write}};
+use std::{net::{TcpListener, TcpStream}, io::{Read, Write}, thread};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
     for stream in listener.incoming() {
         match stream {
-            Ok(mut stream) => {
-                println!("accepted new connection");
-                let mut buf = [0; 512];
-                loop {
-                    let bytes_read = stream.read(&mut buf).unwrap();
-                    if bytes_read == 0 {
-                        print!("Client closed the connection");
-                        break;
-                    }
-                    stream.write("+PONG\r\n".as_bytes()).unwrap();
-                }
+            Ok(stream) => {
+                thread::spawn(move || {
+                    handle_connection(&stream);
+                });
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+}
+
+fn handle_connection(mut stream: &TcpStream) {
+    println!("accepted new connection");
+    let mut buf = [0; 512];
+    loop {
+        let bytes_read = stream.read(&mut buf).unwrap();
+        if bytes_read == 0 {
+            print!("Client closed the connection");
+            break;
+        }
+        stream.write("+PONG\r\n".as_bytes()).unwrap();
+    }
+
 }
