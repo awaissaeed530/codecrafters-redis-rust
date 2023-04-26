19a5a79a399d2c7856959e8f4a052a512bbd20e2
Awais Saeed <awaissaeed530@gmail.com>
2023-04-26 18:19:01 +0500

    actual implementation

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,23 @@
-use std::{net::{TcpListener, TcpStream}, io::{Read, Write}, thread, str};
+mod resp;
 
-fn main() {
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+use anyhow::Result;
+use resp::{
+    RespConnection,
+    Value::{Error, SimpleString},
+};
+use tokio::net::{TcpListener, TcpStream};
 
-    for stream in listener.incoming() {
-        match stream {
-            Ok(stream) => {
-                thread::spawn(move || {
-                    handle_connection(&stream);
+#[tokio::main]
+async fn main() -> Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:6379").await?;
+
+    loop {
+        let incoming = listener.accept().await;
+        match incoming {
+            Ok((stream, _)) => {
+                println!("accepted new connection");
+                tokio::spawn(async move {
+                    handle_connection(stream).await.unwrap();
                 });
             }
             Err(e) => {
@@ -17,39 +27,25 @@ fn main() {
     }
 }
 
-fn handle_connection(mut stream: &TcpStream) {
-    println!("accepted new connection");
-    let mut buf = [0; 512];
+async fn handle_connection(stream: TcpStream) -> Result<()> {
+    let mut conn = RespConnection::new(stream);
+
     loop {
-        let bytes_read = stream.read(&mut buf).unwrap();
-        let req = str::from_utf8(&buf).unwrap();
+        let value = conn.read_value().await?;
 
-        if bytes_read == 0 {
-            print!("Client closed the connection");
+        if let Some(value) = value {
+            let (command, args) = value.to_command()?;
+            let response = match command.to_ascii_lowercase().as_ref() {
+                "ping" => SimpleString("PONG".to_string()),
+                "echo" => args.first().unwrap().clone(),
+                _ => Error(format!("command not implemented: {}", command)),
+            };
+
+            conn.write_value(response).await?;
+        } else {
             break;
-        }
-
-        let mut command = String::new();
-        let mut argument = String::new();
-        let lines = req.lines();
-        if lines.clone().nth(0).unwrap().chars().clone().nth(0).unwrap() == '*' {
-            command.push_str(lines.clone().nth(2).unwrap());
-            argument.push_str(lines.clone().nth(4).unwrap());
-        } 
-
-        match command.as_str() {
-            "ECHO" => {
-                let response = format!("+{}\r\n", argument);
-                stream.write(response.as_bytes()).unwrap();
-
-            }
-            "PING" => {    
-                stream.write("+PONG\r\n".as_bytes()).unwrap();
-            }
-            _ => {
-                stream.write("+UNKNOWN_COMMAND\r\n".as_bytes()).unwrap();
-            }
         }
     }
 
+    Ok(())
 }
