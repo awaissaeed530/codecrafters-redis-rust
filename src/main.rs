468730e7dcf15d7731cb516ea2b69de266c933d5
Awais Saeed <awaissaeed530@gmail.com>
2023-04-26 17:44:50 +0500

    very bad handler for echo command

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{net::{TcpListener, TcpStream}, io::{Read, Write}, thread};
+use std::{net::{TcpListener, TcpStream}, io::{Read, Write}, thread, str};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
@@ -22,11 +22,34 @@ fn handle_connection(mut stream: &TcpStream) {
     let mut buf = [0; 512];
     loop {
         let bytes_read = stream.read(&mut buf).unwrap();
+        let req = str::from_utf8(&buf).unwrap();
+
         if bytes_read == 0 {
             print!("Client closed the connection");
             break;
         }
-        stream.write("+PONG\r\n".as_bytes()).unwrap();
+
+        let mut command = String::new();
+        let mut argument = String::new();
+        let lines = req.lines();
+        if lines.clone().nth(0).unwrap().chars().clone().nth(0).unwrap() == '*' {
+            command.push_str(lines.clone().nth(2).unwrap());
+            argument.push_str(lines.clone().nth(4).unwrap());
+        } 
+
+        match command.as_str() {
+            "ECHO" => {
+                let response = format!("+{}\r\n", argument);
+                stream.write(response.as_bytes()).unwrap();
+
+            }
+            "PING" => {    
+                stream.write("+PONG\r\n".as_bytes()).unwrap();
+            }
+            _ => {
+                stream.write("+UNKNOWN_COMMAND\r\n".as_bytes()).unwrap();
+            }
+        }
     }
 
 }
